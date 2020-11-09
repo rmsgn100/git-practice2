70ad582f2edd01d5a03229409087ddcb6763ce7a
최근후 <choegeunhu@choegeunhuui-MacBookPro.local>
2020-11-09 23:18:35 +0900

    with cal

---
+++ cal.py
@@ -0,0 +1,13 @@
+def add(num1, num2):
+    print(num1+num2)
+def minus(num1, num2):
+    print(num1-num2)
+def multiply(num1, num2):
+    print(num1*num2)
+def divide(num1, num2):
+    print(num1/num2)
+
+add(3, 3)
+minus(3, 2)
+multiply(2, 2)
+divide(4, 2)
