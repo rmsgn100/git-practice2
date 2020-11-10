024ef3f33f4278c2f9d1d8665bce93331bc9ef65
최근후 <choegeunhu@choegeunhuui-MacBookPro.local>
2020-11-10 21:57:46 +0900

    cal.py changed

--- cal.py
+++ cal.py
@@ -1,11 +1,19 @@
+# 더하기 함수
 def add(num1, num2):
-    print(num1+num2)
+    return(num1+num2)
+
+# 빼기 함수
 def minus(num1, num2):
-    print(num1-num2)
+    return(num1-num2)
+
+# 곱하기 합수
 def multiply(num1, num2):
-    print(num1*num2)
+    return(num1*num2)
+
+# 나누기 함수
 def divide(num1, num2):
-    print(num1/num2)
+    return(num1/num2)
+
 
 add(3, 3)
 minus(3, 2)
